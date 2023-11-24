f783d2f3bde4e60a7abe7c7ab8faa8fbef0b4b99
himaloyash <himaloyashis98@gmail.com>
2023-11-25 00:48:42 +0600

    unknown is ignored

--- src/compenents/HomeSection/BigSection/BigSection.jsx
+++ src/compenents/HomeSection/BigSection/BigSection.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import useSomethings from '@/Hoock/useSomethings';
+
 const BigSection = () => {
     return (
         <div>
             <p>Big Section</p>
+            
         </div>
     );
 };
--- src/compenents/HomeSection/HeroSection.jsx
+++ src/compenents/HomeSection/HeroSection.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import useSomethings from '@/Hoock/useSomethings';
+
 
 const HeroSection = () => {
     return (
