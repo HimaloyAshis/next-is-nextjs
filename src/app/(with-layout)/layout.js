7cc6b95cd0a77c97ef93274a412a89befca60e27
himaloyash <himaloyashis98@gmail.com>
2023-12-01 14:42:40 +0600

    navbar ok

--- src/app/(with-layout)/layout.js
+++ src/app/(with-layout)/layout.js
@@ -1,17 +1,11 @@
+import Navbar from '@/compenents/Navbar';
 import Link from 'next/link';
 import React from 'react';
 
-const WithLayout = ({children}) => {
+const WithLayout = ({ children }) => {
     return (
         <div>
-            <nav className='flex  gap-3 text-xl text-purple-400'>
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          {/* <a href="about">About</a> */}
-          <a href="Detail">Detail</a>
-          <a href="product">Product</a>
-        </nav>
-            <p>WithLayout</p>
+            <Navbar></Navbar>
             {children}
             <footer>Foot</footer>
         </div>
--- src/compenents/Navbar.jsx
+++ src/compenents/Navbar.jsx
@@ -1,33 +1,46 @@
+import Link from 'next/link';
 import React from 'react';
 
 const navLink = [
     {
-        path:'/',
-        title:'Home'
+        path: '/',
+        title: 'Home'
     },
     {
-        path:'/about',
-        title:'About'
+        path: '/about',
+        title: 'About'
     },
     {
-        path:'/profile',
-        title:'Profile'
+        path: '/profile',
+        title: 'Profile'
     },
     {
-        path:'/addProduct',
-        title:'AddProduct'
+        path: '/addProduct',
+        title: 'AddProduct'
     },
     {
-        path:'/',
-        title:'Home'
+        path: '/dashboard',
+        title: 'Dashboard'
+    },
+    {
+        path: '/',
+        title: 'Home'
     },
 ]
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className='flex justify-between items-center'>
+            <h1 className='font-semibold text-3xl'>Next Hero</h1>
+            <ul className='flex items-center justify-center gap-3'>
+                {
+                    navLink.map(({ path, title }) =>
+                    <li key={path}>
+                        <Link href={path}>{title}</Link>
+                    </li> )
+                }
+            </ul>
+        </nav>
     );
 };
 
