7f15b0d664ab5b4c096d86bd56072126856db946
himaloyash <himaloyashis98@gmail.com>
2024-02-20 23:24:20 +0600

    nav

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
+const blogs = [
+    {
+        id:1,
+        year:2023,
+        title:"Title 1"
+    },
+    {
+        id:1,
+        year:2023,
+        title:"Title 1"
+    },
+    {
+        id:1,
+        year:2023,
+        title:"Title 1"
+    },
+    {
+        id:1,
+        year:2023,
+        title:"Title 1"
+    },
+]
+
 const BlogPage = () => {
     return (
         <div>
